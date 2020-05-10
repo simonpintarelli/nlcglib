c3d4b582a46fe82fa02fe1ad11348e1c8bc1c211
Simon Pintarelli <simon.pintarelli@cscs.ch>
2020-05-10 14:58:01 +0200

    acc: add cudaMemcpy

---
+++ src/gpu/acc.cpp
@@ -0,0 +1,36 @@
+#include <Kokkos_Complex.hpp>
+
+namespace nlcglib {
+
+#define CALL_DEVICE_API(func__, args__)                      \
+  {                                                          \
+    cudaError_t error;                                       \
+    error = func__ args__;                                   \
+    if (error != cudaSuccess) {                              \
+      std::printf("Error in %s at line %i of file %s: %s\n", \
+                  #func__,                                   \
+                  __LINE__,                                  \
+                  __FILE__,                                  \
+                  cudaGetErrorString(error));                \
+    }                                                        \
+  }
+
+
+template <class T>
+void
+copy(T* dst, const T* src, size_t n)
+{
+  assert(src != nullptr);
+  assert(dst != nullptr);
+  CALL_DEVICE_API(cudaMemcpy, (dst, src, n * sizeof(T)));
+}
+
+template <>
+void
+copy(double*, const double*, size_t);
+
+template <>
+void
+copy(Kokkos::complex<double>*, const Kokkos::complex<double>*, size_t);
+
+}  // namespace nlcglib
--- src/gpu/acc.hpp
+++ src/gpu/acc.hpp
@@ -4,4 +4,7 @@ namespace nlcglib {
 
 int num_devices();
 
+template<typename T>
+void copy(T* target, const T* src, size_t n);
+
 }  // nlcglib
