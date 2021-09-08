061fef02e3a87def6c78f28dc85374fb43e78843
Simon Pintarelli <simon.pintarelli@cscs.ch>
2021-09-08 10:52:13 +0200

    cleanup

--- src/overlap.hpp
+++ src/overlap.hpp
@@ -32,14 +32,6 @@ public:
   auto begin() const { return local::op_iterator<const Overlap>(overlap_base.get_keys(), *this, false); }
   auto end() const { return local::op_iterator<const Overlap>(overlap_base.get_keys(), *this, true); }
 
-  // void test()
-  // {
-  //   key_t k(0,0);
-  //   using pair_t = std::pair<key_t, value_type>;
-  //   pair_t p(k, this->at(k));
-  //   std::unique_ptr<pair_t> x = std::make_unique<pair_t>(k, this->at(k));
-  // }
-
   Communicator commk() const {
     throw std::runtime_error("not implemented");
   }
@@ -54,74 +46,5 @@ Overlap::at(const key_t& key) const -> value_type
 {
   return applicator<OverlapBase>(overlap_base, key);
 }
-
-// class Matrix : public MatrixBaseZ
-// {
-// public:
-//   Matrix(const std::vector<buffer_t>& data,
-//          const std::vector<kindex_t>& indices,
-//          MPI_Comm mpi_comm = MPI_COMM_SELF)
-//       : data(data)
-//       , indices(indices)
-//       , mpi_comm(mpi_comm)
-//   {
-//   }
-
-//   Matrix(std::vector<buffer_t>&& data,
-//           std::vector<kindex_t>&& indices,
-//           MPI_Comm mpi_comm = MPI_COMM_SELF)
-//       : data{std::forward<std::vector<buffer_t>>(data)}
-//       , indices{std::forward<std::vector<kindex_t>>(indices)}
-//       , mpi_comm(mpi_comm)
-//   { /* empty */
-//   }
-
-//   buffer_t get(int i) override { return data[i]; }
-//   const buffer_t get(int i) const override { return data[i]; }
-
-
-//   int size() const override { return data.size(); };
-
-//   MPI_Comm mpicomm(int i) const override { return data[i].mpi_comm; }
-
-//   MPI_Comm mpicomm() const override { return mpi_comm; }
-
-//   kindex_t kpoint_index(int i) const override { return indices[i]; }
-
-// private:
-//   std::vector<buffer_t> data;
-//   std::vector<kindex_t> indices;
-//   MPI_Comm mpi_comm;
-// };
-
-// template <class mvectorX>
-// conditional_add_const_t<Matrix, std::is_const<mvectorX>::value>
-// make_buffer(mvectorX&& X)
-// {
-//   //
-//   std::vector<Matrix::buffer_t> data;
-//   std::vector<Matrix::kindex_t> indices;
-
-//   for (auto elem : X) {
-//     auto x_data = elem.second;
-//     auto key = elem.first;
-
-//     auto mem_t = get_mem_type(x_data);
-//     std::array<int, 2> strides;
-//     strides[0] = x_data.array().stride(0);
-//     strides[1] = x_data.array().stride(1);
-
-//     std::array<int, 2> sizes;
-//     sizes[0] = x_data.array().extent(0);
-//     sizes[1] = x_data.array().extent(1);
-
-//     // TODO: is MPI_COMM_SELF always correct here?
-//     Matrix::buffer_t buf(strides, sizes, x_data.array().data(), mem_t, MPI_COMM_SELF);
-//     indices.push_back(key);
-//     data.push_back(buf);
-//   }
-//   return Matrix(data, indices, X.commk().raw());
-// }
-
 
 }  // namespace nlcglib
