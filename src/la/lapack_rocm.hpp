7023b3ae543a31c6e1cf09180beebb742310d8eb
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-09-03 21:39:03 +0200

    add rocm functions

--- src/la/lapack_rocm.hpp
+++ src/la/lapack_rocm.hpp
@@ -3,22 +3,259 @@
 // #include <rocblas.h>
 // #include <rocsolver.h>
 
+#include <rocblas/internal/rocblas-types.h>
+#include <rocsolver/rocsolver-extra-types.h>
 #include "rocm.hpp"
 #include "rocblas.hpp"
 #include "rocsolver.hpp"
+#include "la/dvector.hpp"
 
 namespace nlcglib {
-namespace rocm {
 
 #ifdef __NLCGLIB__ROCM
-
-
-void my_rocblas_potrfwrapper()
+/// Hermitian eigenvalue problem CUDA
+template <class T, class LAYOUT, class... KOKKOS>
+std::enable_if_t<std::is_same<typename KokkosDVector<T, LAYOUT, KOKKOS...>::storage_t::memory_space,
+                              Kokkos::Experimental::HIPSpace>::value,
+                 void>
+eigh(KokkosDVector<T, LAYOUT, KOKKOS...>& U,
+     Kokkos::View<double*, Kokkos::Experimental::HIPSpace>& w,
+     const KokkosDVector<T, LAYOUT, KOKKOS...>& S)
 {
-  // rocsolver_zpotrf(rocblas_handle handle, const rocblas_fill uplo, const rocblas_int n, rocblas_double_complex *A, const rocblas_int lda, rocblas_int *info);
+  if (U.map().is_local() && S.map().is_local()) {
+
+    deep_copy(U, S);
+
+    int n = U.map().nrows();
+    int lda = U.array().stride(1);
+    rocm::heevd(rocblas_evect::rocblas_evect_original, rocblas_fill::rocblas_fill_upper, n, U.array().data(), lda, w.data());
+  } else {
+    throw std::runtime_error("distributed eigh not implemented");
+  }
+}
+
+/// stores result in RHS, after the call A will contain the cholesky factorization of a
+template <class T, class LAYOUT, class... KOKKOS>
+std::enable_if_t<std::is_same<typename KokkosDVector<T, LAYOUT, KOKKOS...>::storage_t::memory_space, Kokkos::Experimental::HIPSpace>::value>
+cholesky(KokkosDVector<T, LAYOUT, KOKKOS...>& A)
+{
+  if (A.map().is_local()) {
+    // first call potrf
+    int n = A.map().nrows();
+    int lda = A.array().stride(1);
+    auto ptr_A = A.array().data();
+    auto uplo = rocblas_fill::rocblas_fill_upper;
+    int info_potrf;
+    rocm::potrf(uplo, n, ptr_A, lda, info_potrf);
+  } else {
+    throw std::runtime_error("distributed cholesky not implemented");
+  }
+}
+
+
+/// stores result in RHS, after the call A will contain the cholesky factorization of a
+template <class T, class LAYOUT, class... KOKKOS>
+std::enable_if_t<std::is_same<typename KokkosDVector<T, LAYOUT, KOKKOS...>::storage_t::memory_space, Kokkos::Experimental::HIPSpace>::value>
+solve_sym(KokkosDVector<T, LAYOUT, KOKKOS...>& A,
+          KokkosDVector<T, LAYOUT, KOKKOS...>& RHS)
+{
+  if (A.map().is_local() && RHS.map().is_local()) {
+    // first call potrf
+    int n = A.map().nrows();
+    int lda = A.array().stride(1);
+    int ldb = RHS.array().stride(1);
+    auto ptr_B = RHS.array().data();
+    auto ptr_A = A.array().data();
+
+    auto uplo = rocblas_fill::rocblas_fill_upper;
+    int info_potrf;
+    rocm::potrf(uplo, n, ptr_A, lda, info_potrf);
+    int nrhs = RHS.array().extent(1);
+
+    rocm::potrs(uplo, n, nrhs, ptr_A, lda, ptr_B, ldb);
+  } else {
+    throw std::runtime_error("distributed solve_sym not implemented");
+  }
+}
+
+/// Inner product c = a^H * b, on GPU
+template <class M0, class M1, class M2>
+std::enable_if_t<std::is_same<typename M0::storage_t::memory_space, Kokkos::Experimental::HIPSpace>::value, void>
+inner(M0& c,
+      const M1& a,
+      const M2& b,
+      const typename M0::numeric_t& alpha = typename M0::numeric_t{1.0},
+      const typename M0::numeric_t& beta = typename M0::numeric_t{0.0})
+{
+  typedef typename M1::storage_t::value_type numeric_t;
+
+  static_assert(std::is_same<typename M0::storage_t::memory_space,
+                             typename M1::storage_t::memory_space>::value,
+                "c,a not on same memory");
+  static_assert(std::is_same<typename M1::storage_t::memory_space,
+                             typename M2::storage_t::memory_space>::value,
+                "a,b not on same memory");
+  // static_assert(std::is_same<LAYOUT1, LAYOUT2>::value, "matrix layout do not match");
+  if (a.map().is_local() && b.map().is_local() && c.map().is_local()) {
+    if (a.array().stride(0) != 1 || b.array().stride(0) != 1 || c.array().stride(0) != 1) {
+      throw std::runtime_error("expecting column major layout");
+    }
+
+    int m = a.map().ncols();
+    int k = a.map().nrows();
+    int n = b.map().ncols();
+    numeric_t* A_ptr = a.array().data();
+    numeric_t* B_ptr = b.array().data();
+    numeric_t* C_ptr = c.array().data();
+
+    int lda = a.array().stride(1);
+    int ldb = b.array().stride(1);
+    int ldc = c.array().stride(1);
+
+    auto H = rocblas_operation::rocblas_operation_conjugate_transpose;
+    auto N = rocblas_operation::rocblas_operation_none;
+    rocm::gemm(H, N, m, n, k, alpha, A_ptr, lda, B_ptr, ldb, beta, C_ptr, ldc);
+  } else {
+    throw std::runtime_error("distributed inner product not implemented.");
+  }
+}
+
+/// Inner product c = a^H * b, on GPU
+template <class M0,
+          class M1,
+          class M2>
+std::enable_if_t<
+    std::is_same<typename M0::storage_t::memory_space,
+                 Kokkos::Experimental::HIPSpace>::value,
+    void>
+outer(M0& c,
+      const M1& a,
+      const M2& b,
+      const typename M0::numeric_t& alpha = typename M0::numeric_t{1.0},
+      const typename M0::numeric_t& beta = typename M0::numeric_t{0.0})
+{
+  typedef M0 vector0_t;
+  typedef M1 vector1_t;
+  typedef M2 vector2_t;
+  typedef typename vector1_t::storage_t::value_type numeric_t;
+
+  static_assert(std::is_same<typename vector0_t::storage_t::memory_space,
+                             typename vector1_t::storage_t::memory_space>::value,
+                "c,a not on same memory");
+  static_assert(std::is_same<typename vector1_t::storage_t::memory_space,
+                             typename vector2_t::storage_t::memory_space>::value,
+                "a,b not on same memory");
+  // static_assert(std::is_same<LAYOUT1, LAYOUT2>::value, "matrix layout do not match");
+  if (a.map().is_local() && b.map().is_local() && c.map().is_local()) {
+    if (a.array().stride(0) != 1 || b.array().stride(0) != 1 || c.array().stride(0) != 1) {
+      throw std::runtime_error("expecting column major layout");
+    }
+
+    int m = a.map().ncols();
+    int k = a.map().nrows();
+    int n = b.map().ncols();
+    numeric_t* A_ptr = a.array().data();
+    numeric_t* B_ptr = b.array().data();
+    numeric_t* C_ptr = c.array().data();
+
+    int lda = a.array().stride(1);
+    int ldb = b.array().stride(1);
+    int ldc = c.array().stride(1);
+    auto H = rocblas_operation::rocblas_operation_conjugate_transpose;
+    auto N = rocblas_operation::rocblas_operation_none;
+
+    rocm::gemm(N, H, m, n, k, alpha, A_ptr, lda, B_ptr, ldb, beta, C_ptr, ldc);
+
+  } else {
+    throw std::runtime_error("distributed outer product not implemented.");
+  }
+}
+
+/// C <- beta * C + alpha * A @ B
+template <class M0, class M1, class M2>
+std::enable_if_t<std::is_same<typename M0::storage_t::memory_space, Kokkos::Experimental::HIPSpace>::value, void>
+transform(M0& C,
+          typename M0::numeric_t beta,
+          typename M0::numeric_t alpha,
+          const M1& A,
+          const M2& B)
+{
+  typedef M0 vector0_t;
+  typedef M1 vector1_t;
+  typedef M2 vector2_t;
+  typedef typename vector1_t::storage_t::value_type numeric_t;
+
+  static_assert(std::is_same<typename vector0_t::storage_t::memory_space,
+                             typename vector1_t::storage_t::memory_space>::value,
+                "c,a not on same memory");
+  static_assert(std::is_same<typename vector1_t::storage_t::memory_space,
+                             typename vector2_t::storage_t::memory_space>::value,
+                "a,b not on same memory");
+  // static_assert(std::is_same<LAYOUT1, LAYOUT2>::value, "matrix layout do not match");
+
+  if (A.map().is_local() && B.map().is_local() && C.map().is_local()) {
+    /* single rank */
+    int m = A.map().nrows();
+    int n = B.map().ncols();
+    int k = A.map().ncols();
+    numeric_t* A_ptr = A.array().data();
+    numeric_t* B_ptr = B.array().data();
+    numeric_t* C_ptr = C.array().data();
+
+    if (A.array().stride(0) != 1 || B.array().stride(0) != 1 || C.array().stride(0) != 1) {
+      throw std::runtime_error("expecting column major layout");
+    }
+    // assume there are no strides
+    int lda = A.array().stride(1);
+    int ldb = B.array().stride(1);
+    int ldc = C.array().stride(1);
+
+    auto N = rocblas_operation::rocblas_operation_none;
+    rocm::gemm(N, N, m, n, k, alpha, A_ptr, lda, B_ptr, ldb, beta, C_ptr, ldc);
+  } else {
+    throw std::runtime_error("distributed transform not implemented.");
+  }
+}
+
+
+/// add C <- alpha * A + beta * C
+template <class M0, class M1>
+std::enable_if_t<std::is_same<typename M0::storage_t::memory_space, Kokkos::Experimental::HIPSpace>::value, void>
+add(M0& C,
+    const M1& A,
+    typename M0::numeric_t alpha,
+    typename M0::numeric_t beta = typename M0::numeric_t{1.0})
+{
+  typedef M0 vector0_t;
+  typedef M1 vector1_t;
+  typedef typename vector1_t::storage_t::value_type numeric_t;
+
+  static_assert(std::is_same<typename vector0_t::storage_t::memory_space,
+                             typename vector1_t::storage_t::memory_space>::value,
+                "c,a not on same memory");
+
+  if (A.map().is_local() && C.map().is_local()) {
+    /* single rank */
+    int m = A.map().nrows();
+    int n = C.map().ncols();
+    numeric_t* A_ptr = A.array().data();
+    numeric_t* C_ptr = C.array().data();
+
+    if (A.array().stride(0) != 1 || C.array().stride(0) != 1) {
+      throw std::runtime_error("expecting column major layout");
+    }
+    // assume there are no strides
+    int lda = A.array().stride(1);
+    int ldc = C.array().stride(1);
+
+    // using geam = rocm::geam<numeric_t>;
+    auto N = rocblas_operation::rocblas_operation_none;
+    rocm::geam(N, N, m, n, alpha, A_ptr, lda, beta, C_ptr, ldc);
+  } else {
+    throw std::runtime_error("not implemented.");
+  }
 }
 
 #endif
 
-}  // rocm
 }  // nlcglib
--- src/la/rocm.hpp
+++ src/la/rocm.hpp
@@ -6,16 +6,38 @@
 #include "la/lapack_rocm.hpp"
 #include "rocblas.hpp"
 #include "rocsolver.hpp"
+#include <cstdlib>
+#include <cstdio>
+#include "backtrace.hpp"
+
+
+#define CALL_ROCBLAS(func__, args__)                           \
+  {                                                         \
+    rocblas_status status = func__ args__;                  \
+    if (status != rocblas_status::rocblas_status_success) { \
+      char nm[1024];                                        \
+      gethostname(nm, 1024);                                \
+      printf("hostname: %s\n", nm);                         \
+      printf("Error in %s at line %i of file %s: %s\n",     \
+             #func__,                                       \
+             __LINE__,                                      \
+             __FILE__,                                      \
+             rocblas_status_to_string(status));             \
+      stack_backtrace();                                    \
+    }                                                       \
+  }
 
 namespace nlcglib {
 namespace rocm {
+
+
 
 // use rocblas_status_to_string(rocblas_status status)
 
 #ifdef __NLCGLIB__ROCM
 
 template <class T>
-void
+inline void
 potrf(rocblas_fill uplo, int n, T* A, int lda, int& Info)
 {
   static_assert(std::is_same<T, std::complex<double>>::value ||
@@ -25,17 +47,12 @@ potrf(rocblas_fill uplo, int n, T* A, int lda, int& Info)
   rocblas_double_complex* A_ptr = reinterpret_cast<rocblas_double_complex*>(A);
 
   rocblas_int* dev_info;
-  auto status = rocsolver_zpotrf(handle, uplo, n, A_ptr, lda, dev_info);
-
-  if (status != rocblas_status::rocblas_status_success) {
-    // exit
-    throw std::runtime_error("rocsolver_zpotrf failed");
-  }
+  CALL_ROCBLAS(rocsolver_zpotrf, (handle, uplo, n, A_ptr, lda, dev_info))
 }
 
 
 template <class T>
-void
+inline void
 potrs(rocblas_fill uplo, int n, int nrhs, T* A, int lda, T* B, int ldb)
 {
   static_assert(std::is_same<T, std::complex<double>>::value ||
@@ -46,17 +63,12 @@ potrs(rocblas_fill uplo, int n, int nrhs, T* A, int lda, T* B, int ldb)
   rocblas_double_complex* A_ptr = reinterpret_cast<rocblas_double_complex*>(A);
   rocblas_double_complex* B_ptr = reinterpret_cast<rocblas_double_complex*>(B);
 
-  auto status = rocsolver_zpotrs(handle, uplo, n, nrhs, A_ptr, lda, B_ptr, ldb);
+  CALL_ROCBLAS(rocsolver_zpotrs, (handle, uplo, n, nrhs, A_ptr, lda, B_ptr, ldb))
 
-  if (status != rocblas_status::rocblas_status_success) {
-    // exit
-    throw std::runtime_error("rocsolver_zpotrs failed");
-  }
 }
 
-
 template <class T>
-void
+inline void
 heevd(rocblas_evect mode, rocblas_fill uplo, int n, T* A, int lda, double* w)
 {
   static_assert(std::is_same<T, std::complex<double>>::value ||
@@ -75,24 +87,16 @@ heevd(rocblas_evect mode, rocblas_fill uplo, int n, T* A, int lda, double* w)
   double* E;
 
   // TODO: make a wrapper for hipMalloc
-  auto res = hipMalloc(&E, n);
+  hipError_t res = hipMalloc(&E, n);
 
-  auto status = rocsolver_zheevd(handle, mode, uplo, n, A_ptr, lda, w, E, dev_info);
-
-  hipDeviceSynchronize();
+  CALL_ROCBLAS(rocsolver_zheevd, (handle, mode, uplo, n, A_ptr, lda, w, E, dev_info))
 
   int info;
   hipMemcpyDtoH(&info, dev_info, sizeof(int));
-
-  if (status != rocblas_status::rocblas_status_success) {
-    // exit
-    throw std::runtime_error("rocsolver_zpotrs failed, info=" + std::to_string(info));
-  }
-
   hipFree(E);
 }
 
-void
+inline void
 gemm(rocblas_operation transa,
      rocblas_operation transb,
      int m,
@@ -101,37 +105,47 @@ gemm(rocblas_operation transa,
      Kokkos::complex<double> alpha,
      const Kokkos::complex<double>* A,
      int lda,
-     Kokkos::complex<double> beta,
      const Kokkos::complex<double>* B,
      int ldb,
+     Kokkos::complex<double> beta,
      Kokkos::complex<double>* C,
      int ldc)
 {
   auto handle = rocblasHandle::get();
-  auto info = rocblas_zgemm(handle,
-                            transa,
-                            transb,
-                            m,
-                            n,
-                            k,
-                            reinterpret_cast<const rocblas_double_complex*>(&alpha),
-                            reinterpret_cast<const rocblas_double_complex*>(A),
-                            lda,
-                            reinterpret_cast<const rocblas_double_complex*>(B),
-                            ldb,
-                            reinterpret_cast<const rocblas_double_complex*>(&beta),
-                            reinterpret_cast<rocblas_double_complex*>(C),
-                            ldc);
 
-  if (info != rocblas_status::rocblas_status_success) {
-    throw std::runtime_error("rocblas_zgemm failed\n");
-  }
+  CALL_ROCBLAS(rocblas_zgemm,
+               (handle,
+                transa,
+                transb,
+                m,
+                n,
+                k,
+                reinterpret_cast<const rocblas_double_complex*>(&alpha),
+                reinterpret_cast<const rocblas_double_complex*>(A),
+                lda,
+                reinterpret_cast<const rocblas_double_complex*>(B),
+                ldb,
+                reinterpret_cast<const rocblas_double_complex*>(&beta),
+                reinterpret_cast<rocblas_double_complex*>(C),
+                ldc))
 }
 
-void geam()
+template<class T>
+inline void geam(rocblas_operation transa,
+                 rocblas_operation transb,
+                 int m,
+                 int n,
+                 T alpha,
+                 T* A,
+                 int lda,
+                 T beta,
+                 T* C,
+                 int ldc)
 {
   auto handle = rocblasHandle::get();
+  throw std::runtime_error("NOT IMPLEMENTED!");
 
+  // have to use axpy here ...
   // somehow this is missing..
   // rocblas_zgeam
 }
