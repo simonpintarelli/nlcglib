e8be85f088befed9d60f3bb6d60eb80b048b2038
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-02-13 16:21:44 +0100

    add CUDA::cuda_driver target

--- cmake/cudalibs_target.cmake
+++ cmake/cudalibs_target.cmake
@@ -1,5 +1,5 @@
 find_package(CUDAToolkit REQUIRED)
 if (NOT TARGET nlcglib::cudalibs)
   add_library(nlcglib::cudalibs INTERFACE IMPORTED)
-  target_link_libraries(nlcglib::cudalibs INTERFACE CUDA::cudart CUDA::cublas CUDA::cusolver)
+  target_link_libraries(nlcglib::cudalibs INTERFACE CUDA::cudart CUDA::cuda_driver CUDA::cublas CUDA::cusolver)
 endif()
