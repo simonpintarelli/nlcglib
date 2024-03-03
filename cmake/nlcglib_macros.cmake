2ae1a6b879a8a0d53d2a9afb8f5d9d3b453abdc3
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-03-03 15:21:47 +0100

    cmake: simplify blas/mkl

--- cmake/nlcglib_macros.cmake
+++ cmake/nlcglib_macros.cmake
@@ -2,40 +2,30 @@ MACRO(NLCGLIB_SETUP_TARGET _target)
   target_link_libraries(
     ${_target} PUBLIC
     Kokkos::kokkos
-    # ${LAPACK_LIBRARIES}
     MPI::MPI_CXX
-    # $<TARGET_NAME_IF_EXISTS:OpenMP::OpenMP_CXX>
+    $<TARGET_NAME_IF_EXISTS:OpenMP::OpenMP_CXX>
     $<TARGET_NAME_IF_EXISTS:nlcglib::cudalibs>
     $<TARGET_NAME_IF_EXISTS:nlcglib::rocmlibs>
     $<TARGET_NAME_IF_EXISTS:nlcglib::magma>
     $<TARGET_NAME_IF_EXISTS:roc::hipblas> # only required for magma
     $<TARGET_NAME_IF_EXISTS:roc::hipsparse> # only required for magma
     nlohmann_json::nlohmann_json
-    )
+    nlcg::cpu_lapack
+  )
 
   target_include_directories(${_target} PUBLIC
     ${CMAKE_SOURCE_DIR}/src
     ${CMAKE_SOURCE_DIR}/include
-    )
+  )
 
-    if(USE_ROCM)
-      target_compile_options(${_target} PUBLIC --offload-arch=gfx90a)
-    endif()
-
-  if(LAPACK_VENDOR STREQUAL MKL)
-    target_compile_definitions(${_target} PUBLIC __USE_MKL)
-    target_link_libraries(${_target}  PUBLIC mkl::mkl_intel_32bit_omp_dyn)
-  elseif(LAPACK_VENDOR STREQUAL MKLONEAPI)
-    target_compile_definitions(${_target}  PUBLIC __USE_MKL)
-    target_link_libraries(${_target}  PUBLIC MKL::MKL)
-  else()
-    target_link_libraries(${_target} PRIVATE nlcg::cpu_lapack)
+  if(USE_ROCM)
+    target_compile_options(${_target} PUBLIC --offload-arch=gfx90a)
   endif()
+
   target_compile_definitions(${_target} PUBLIC $<$<BOOL:${USE_OPENMP}>:__USE_OPENMP>)
   target_compile_definitions(${_target} PUBLIC $<$<BOOL:${USE_CUDA}>:__NLCGLIB__CUDA>)
   target_compile_definitions(${_target} PUBLIC $<$<BOOL:${USE_ROCM}>:__NLCGLIB__ROCM>)
   target_compile_definitions(${_target} PUBLIC $<$<BOOL:${USE_MAGMA}>:__NLCGLIB__MAGMA>)
   target_compile_definitions(${_target} PUBLIC $<$<BOOL:${USE_GPU_DIRECT}>:__NLCGLIB__GPU_DIRECT>)
   target_include_directories(${_target} PUBLIC $<TARGET_PROPERTY:Kokkos::kokkoscore,INTERFACE_INCLUDE_DIRECTORIES>)
-
 ENDMACRO()
