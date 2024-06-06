957951c1ab49dde30699232e5b1ca4ac7468be3e
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-06-06 09:48:24 +0200

    remove kokkos <4 initialization

--- spack/packages/nlcglib/package.py
+++ spack/packages/nlcglib/package.py
@@ -39,6 +39,10 @@ class Nlcglib(CMakePackage, CudaPackage, ROCmPackage):
 
     depends_on("googletest", type="build", when="+tests")
     depends_on("nlohmann-json")
+    depends_on("kokkos@4:", when="@1.1:")
+
+    # MKLConfig.cmake introduced in 2021.3
+    conflicts("intel-oneapi-mkl@:2021.2", when="^intel-oneapi-mkl")
 
     with when("@:0.9"):
         conflicts("+rocm")
@@ -113,8 +117,6 @@ class Nlcglib(CMakePackage, CudaPackage, ROCmPackage):
                 options += [self.define("CMAKE_CUDA_FLAGS", cuda_flags)]
             else:
                 options += [self.define("CMAKE_CUDA_ARCHITECTURES", cuda_archs)]
-
-
 
         if "^cuda+allow-unsupported-compilers" in self.spec:
             options += [self.define("CMAKE_CUDA_FLAGS", "--allow-unsupported-compiler")]
--- src/nlcglib.cpp
+++ src/nlcglib.cpp
@@ -26,16 +26,8 @@ namespace nlcglib {
 void
 initialize()
 {
-#if KOKKOS_VERSION < 30700
-  Kokkos::InitArguments args;
-  args.disable_warnings = true;
-#ifdef USE_OPENMP
-  args.num_threads = omp_get_max_threads();
-#endif /* endif USE_OPENMP */
-#else  /* KOKKOS_VERSION >= 3.7.00 */
   Kokkos::InitializationSettings args;
   args.set_disable_warnings(true);
-#endif /* endif KOKKOS VERSION */
 #ifdef USE_OPENMP
   args.num_threads = omp_get_max_threads();
 #endif
