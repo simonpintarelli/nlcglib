b6ed37743395f27c03735d1c471dc33c75b33d22
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-04-13 17:25:25 +0200

    GPU acc cleanup

--- src/gpu/acc.cpp
+++ src/gpu/acc.cpp
@@ -1,49 +1,23 @@
-#ifdef __NLCGLIB__CUDA
-#include <cuda.h>
+#include "gpu/acc.hpp"
+#if defined(__NLCGLIB__CUDA)
 #include <cuda_runtime_api.h>
-#include <Kokkos_Complex.hpp>
-#include <cassert>
-#include <complex>
-#include <cstdio>
+#endif
+
+#if defined(__NLCGLIB__ROCM)
+#include <hip/hip_runtime_api.h>
+#endif
 
 namespace nlcglib {
-#define CALL_DEVICE_API(func__, args__)                      \
-  {                                                          \
-    cudaError_t error;                                       \
-    error = func__ args__;                                   \
-    if (error != cudaSuccess) {                              \
-      std::printf("Error in %s at line %i of file %s: %s\n", \
-                  #func__,                                   \
-                  __LINE__,                                  \
-                  __FILE__,                                  \
-                  cudaGetErrorString(error));                \
-    }                                                        \
-  }
-
 namespace acc {
 
-template <class T, class U = T>
-void
-copy(T* dst, const U* src, size_t n)
+acc_meminfo
+get_mem_info()
 {
-  assert(src != nullptr);
-  assert(dst != nullptr);
-  CALL_DEVICE_API(cudaMemcpy, (dst, src, n * sizeof(T), cudaMemcpyDefault));
+  acc_meminfo info{};
+  CALL_DEVICE_API(MemGetInfo, (&info.free, &info.total));
+  return info;
 }
-
-template void
-copy(double*, const double*, size_t);
-
-template void
-copy(Kokkos::complex<double>*, const Kokkos::complex<double>*, size_t);
-
-template void
-copy(std::complex<double>*, const std::complex<double>*, size_t);
-
-template void
-copy(Kokkos::complex<double>*, const std::complex<double>*, size_t);
 
 }  // namespace acc
 
 }  // namespace nlcglib
-#endif
